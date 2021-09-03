b31b1278e99f1c186e1ba4a8b6dc3cd078a63b22
Drinoff <drinoff@gmail.com>
2021-09-03 11:20:14 +0200

    releases page and home page done

--- src/views/releasesPage.js
+++ src/views/releasesPage.js
@@ -3,18 +3,32 @@ import { getReleases } from '../api/data.js'
 
 
 const releasesTemplate = (data) => html`
-    <div>releases</div>
+    <section class="releasesWrapper">
+        ${data.map(releaseCard)}
+    
+    </section>
 `;
 
 const releaseCard = (item) => html`
-
-
+    <article class="singleRelease">
+        <a href=${item.bandCamp} target="_blank">
+            <div class="imgWrapper">
+                <img class="releaseImg" src=${item.URL} alt="releaseImage" />
+            </div>
+            <div class="releaseInfo">
+                <p>${item.artist}</p>
+                <p>${item.track}</p>
+            </div>
+        </a>
+    </article>
+   
 
 `;
 
 export async function releasesPage(ctx) {
 
     let data = await getReleases();
+    console.log(data[0])
 
     ctx.render(releasesTemplate(data));
 
