ada4c3e0e5c1cc88acece6937d66cc6f8378f836
Drinoff <drinoff@gmail.com>
2021-09-03 13:36:43 +0200

    contact page push

--- src/views/contactPage.js
+++ src/views/contactPage.js
@@ -1,11 +1,23 @@
 import { html } from 'https://unpkg.com/lit-html?module';
 
 const contactTemplate = () => html`
-    <div>contact</div>
+    <section class="contatc">
+        <article class="contactUS">
+            <h1>Contact:</h1>
+            <p>For demos, bookings or any inquiries, please contact <a class="mailto" href="mailto:fieldconspiracy@gmail.com">fieldconspiracy@gmail.com</a> 
+            </p>
+        </article>
+        <article class="about">
+            <h1>About:</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque cum non eum voluptates nulla corporis fuga
+                earum vitae facere? Est.
+            </p>
+        </article>
+    </section>
 `;
 
 
 export async function contactPage(ctx) {
     ctx.render(contactTemplate());
-    
+
 }
