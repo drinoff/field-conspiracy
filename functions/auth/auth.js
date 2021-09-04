c5b510ad352a9dd7f88e700b97aabf6e5b8acf4b
Drinoff <drinoff@gmail.com>
2021-09-04 13:25:21 +0200

    netlify function

--- functions/auth/auth.js
+++ functions/auth/auth.js
@@ -1,28 +1,12 @@
+// for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
 
-const fetch = require('node-fetch');
-exports.handler = async (event, context) => {
-  console.log(event)
-  //   Only allow POST
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
-  }
-  const errorGen = msg => {
-    return { statusCode: 500, body: msg };
-  };
+const fetch = require('node-fetch')
+
+const handler = async function (event, context) {
+  const { email_address, pass} = JSON.parse(event.body);
+    
+  
   try {
-    const { email_address, pass} = JSON.parse(event.body);
-    //const email = email_address;
-    //const password = pass;
-    if (!email_address) {
-      return errorGen('Missing Email');
-    }
-    if (!pass) {
-      return errorGen('Missing Password');
-    }
-    const subscriber = {
-      email: email_address,
-      password: pass, 
-    };
     const API = `any:${process.env.FIREBASE_AUTH}`;
     const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API}`, {
       method: 'POST',
@@ -33,21 +17,26 @@ exports.handler = async (event, context) => {
       },
       body: JSON.stringify(subscriber),
     });
-    const data = await response.text();
-    
-    // if (!response.ok) {
-    //   // NOT res.status >= 200 && res.status < 300 
-    //   return { statusCode: data.statusCode, body: data.detail };
-    // }
+
+    const data = await response.json();
+    if (!response.ok) {
+      // NOT res.status >= 200 && res.status < 300
+      return { statusCode: response.status, body: response.statusText }
+    }
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ msg: "Authenticated", detail: data, }),
-    };
-  } catch (err) {
-    console.log(err); // output to netlify function log 
+      body: JSON.stringify({ identity, user, msg: data.value }),
+    }
+  } catch (error) {
+    // output to netlify function log
+    console.log(error)
     return {
       statusCode: 500,
-      body: JSON.stringify({msg: err.message }),
-    };
+      // Could be a custom message or object i.e. JSON.stringify(err)
+      body: JSON.stringify({ msg: error.message }),
+    }
   }
-};
+}
+
+module.exports = { handler }
