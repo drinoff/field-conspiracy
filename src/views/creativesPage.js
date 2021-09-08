34f054093cb626938b74760125ccf8650460c521
Drinoff <drinoff@gmail.com>
2021-09-08 09:02:16 +0200

    creatives page initial commit

--- src/views/creativesPage.js
+++ src/views/creativesPage.js
@@ -1,21 +1,31 @@
 import { html } from 'https://unpkg.com/lit-html?module';
-import { getCreatives } from '../api/data.js'
-
+import { getCreatives } from '../api/data.js';
 
 const creativesTemplate = (data) => html`
-    <div>creatives</div>
+
+<section class="artistsWrapper">
+    ${data.map(creativeCard)}
+</section>
+
 `;
 
-const creativesCard = (item) => html`
+const creativeCard = (item) => html`
+<article class="singleArtist">
 
+    <div class="imgWrapper">
+        <img class="artistImg" src=${item[1].img} alt="artistImage" />
+    </div>
+    <div class="artistInfo">
+        <p>${item[1].name}</p>
+    </div>
 
+</article>
 
 `;
 
 export async function creativesPage(ctx) {
-
-    let data = await getCreatives();
-
+    let dataObj = await getCreatives();
+    let data = Object.entries(dataObj)
+    console.log(data)
     ctx.render(creativesTemplate(data));
-
 }
