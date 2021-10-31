f4db7510ccd24506699a8594a591ffa160126b0b
Drinoff <drinoff@gmail.com>
2021-10-31 07:19:59 +0100

    not found page implemented

--- src/views/notFound.js
+++ src/views/notFound.js
@@ -2,7 +2,8 @@ import { html } from 'https://unpkg.com/lit-html?module';
 
 const notFound = () => html `
 
-<p>Not Found</p>
+<img src="../../assets/notFound.png" alt="notFoundImg" />
+<p style="font-size:22px">The Resource you looking for is Not Found</p>
 `;
 
 export function notFoundPage(ctx) {
